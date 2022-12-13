c4939fd4ec6cfe427f25434523ab821d6619c84f
Acors24 <acors@protonmail.com>
2022-12-13 17:25:04 +0100

    change colouring method

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from json import load
 import matplotlib.pyplot as plt
 from colorsys import hsv_to_rgb
 from datetime import date
+from math import ceil
 
 with open('data.json') as f:
     json = load(f)
@@ -60,9 +61,16 @@ else:
 
 fig, ax = plt.subplots(layout="constrained")
 styles = ['-', '--', '-.', ':']
+hue_interval = 360 / ceil(len(ind_scores) / len(styles))
 for i, score_list in enumerate(ind_scores.values()):
     accumulate(score_list)
-    ax.plot(range(1, 26), score_list, linestyle=styles[i % len(styles)], color=hsv_to_rgb(i * 77 / 360, 1, 0.9))
+    style = styles[i % len(styles)]
+    hue = i // len(styles) * hue_interval / 360
+    ax.plot(
+        range(1, 26), score_list,
+        linestyle=style,
+        color=hsv_to_rgb(hue, 1, 1)
+    )
     ax.set_xbound(1, last_day)
 
 ax.legend([data['name'] for _, data in members.items()], bbox_to_anchor=(1.01, 1), loc='upper left', borderaxespad=0)
