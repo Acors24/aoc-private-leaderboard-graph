608dec492cc87039c61e1a2d30f4ceba6114be1c
Acors24 <acors@protonmail.com>
2022-12-13 16:26:10 +0100

    add automatic setting of upper xbound for the plot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from json import load
 import matplotlib.pyplot as plt
 from colorsys import hsv_to_rgb
+from datetime import date
 
 with open('data.json') as f:
     json = load(f)
@@ -51,13 +52,18 @@ def accumulate(scores: list[int]):
         scores[i] += scores[i - 1]
 
 
-end = 25 # Hide unavailable days by setting this value to the amount of days unlocked
+today = date.today()
+if today.month == 12 and today.day < 25:
+    last_day = today.day
+else:
+    last_day = 25
+
 fig, ax = plt.subplots(layout="constrained")
 styles = ['-', '--', '-.', ':']
 for i, score_list in enumerate(ind_scores.values()):
     accumulate(score_list)
     ax.plot(range(1, 26), score_list, linestyle=styles[i % len(styles)], color=hsv_to_rgb(i * 77 / 360, 1, 0.9))
-    ax.set_xbound(1, end)
+    ax.set_xbound(1, last_day)
 
 ax.legend([data['name'] for _, data in members.items()], bbox_to_anchor=(1.01, 1), loc='upper left', borderaxespad=0)
 plt.show()
